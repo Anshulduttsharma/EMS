bcf60eb0897716421cba2567ae2f4d34393c9f6e
Anshul <anshuls26@gmail.com>
2025-03-20 18:27:01 +0530

    login functionality added and tailwind css fixed

---
+++ src/components/Auth/Login.jsx
@@ -0,0 +1,51 @@
+import React, { use, useState } from "react";
+
+const Login = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const submitHandler = (e) => {
+    e.preventDefault();
+    console.log("email is : ", email);
+    console.log("Password is : ", password);
+
+    setEmail("")
+    setPassword("")
+  };
+  return (
+    <div className="flex h-screen w-screen items-center justify-center">
+      <div className="border-2 border-emerald-600 p-20">
+        <form
+          onSubmit={submitHandler}
+          className="flex flex-col items-center justify-center"
+        >
+          <input
+            required
+            value={email}
+            onChange={(e) => {
+              setEmail(e.target.value);
+            }}
+            className="text-white outline-none border-2 border-emerald-600 rounded-full py-3 px-5 placeholder:text-gray-400"
+            type="email"
+            placeholder="Enter Your Email"
+          />
+          <input
+            required
+            value={password}
+            onChange={(e) => {
+              setPassword(e.target.value);
+            }}
+            className="text-white outline-none mt-3 border-2 border-emerald-600 rounded-full py-3 px-5 placeholder:text-gray-400"
+            type="password"
+            placeholder="Enter Your Password"
+          />
+          <button className="text-white outline-none bg-emerald-600 mt-3  rounded-full py-3 px-5">
+            Log in
+          </button>
+        </form>
+      </div>
+    </div>
+  );
+};
+
+export default Login;
