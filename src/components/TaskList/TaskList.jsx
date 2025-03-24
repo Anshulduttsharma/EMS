c9381ca0e9112dfa4dcc55bda919813fda0edbba
Anshul <anshuls26@gmail.com>
2025-03-24 13:45:24 +0530

    update employee dashboard page UI

---
+++ src/components/TaskList/TaskList.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+const TaskList = () => {
+  return (
+    <div id="taskList" className='h-[55%] flex overflow-x-auto items-center gap-5 justify-start flex-nowrap w-full mt-10 rounded-xl'>
+      <div className='flex-shrink-0 h-full w-[300px] bg-yellow-400 rounded-xl'></div>
+      <div className='flex-shrink-0 h-full w-[300px] bg-yellow-400 rounded-xl'></div>
+      <div className='flex-shrink-0 h-full w-[300px] bg-yellow-400 rounded-xl'></div>
+      <div className='flex-shrink-0 h-full w-[300px] bg-yellow-400 rounded-xl'></div>
+      <div className='flex-shrink-0 h-full w-[300px] bg-yellow-400 rounded-xl'></div>
+      <div className='flex-shrink-0 h-full w-[300px] bg-yellow-400 rounded-xl'></div>
+      <div className='flex-shrink-0 h-full w-[300px] bg-yellow-400 rounded-xl'></div>
+      <div className='flex-shrink-0 h-full w-[300px] bg-yellow-400 rounded-xl'></div>
+    </div>
+  )
+}
+
+export default TaskList
