66d04e4c63184ed4845bcaac963b7238e373760b
Anshul <anshuls26@gmail.com>
2025-03-25 14:04:29 +0530

    update loggedInUser functionality

--- src/App.jsx
+++ src/App.jsx
@@ -2,23 +2,39 @@ import "./App.css";
 import Login from "./components/Auth/Login";
 import EmployeeDashboard from "./components/Dashboard/EmployeeDashboard";
 import AdminDashboard from "./components/Dashboard/AdminDashboard";
-import { useContext, useState } from "react";
+import { useContext, useEffect, useState } from "react";
 import { AuthContext } from "./context/AuthProvider";
 
 function App() {
   const [user, setUser] = useState(null);
-  const AuthData = useContext(AuthContext)
+  const AuthData = useContext(AuthContext);
+
+  useEffect(() => {
+    if (AuthData) {
+      const loggedInUser = localStorage.getItem("loggedInUser");
+      if(loggedInUser){
+        setUser(loggedInUser.role)
+      }
+    }
+  }, [AuthData]);
 
   const handleLogin = (email, password) => {
     if (email == "admin@me.com" && password == 123) {
       setUser("admin");
-    } else if (AuthData && AuthData.employees.find((e)=> email == e.email && password == e.password)) {
+      localStorage.setItem("loggedInUser", JSON.stringify({ role: "admin" }));
+    } else if (
+      AuthData &&
+      AuthData.employees.find((e) => email == e.email && password == e.password)
+    ) {
       setUser("employee");
+      localStorage.setItem(
+        "loggedInUser",
+        JSON.stringify({ role: "employee" })
+      );
     } else {
       alert("Invalid Credentials!");
     }
   };
-  
 
   return (
     <>
